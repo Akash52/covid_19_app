2acb4d6b42207267fe522a44af8899b1a8cb7aed
Akash Chauhan <19it197@gmail.com>
2020-05-17 12:03:31 +0530

    API Done

--- app/src/main/java/com/example/covid_19tracker/MainActivity.java
+++ app/src/main/java/com/example/covid_19tracker/MainActivity.java
@@ -11,13 +11,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.leo.simplearcloader.SimpleArcLoader;
 
 import org.eazegraph.lib.charts.PieChart;
 import org.eazegraph.lib.models.PieModel;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -42,24 +46,9 @@ public class MainActivity extends AppCompatActivity {
 
         simpleArcLoader=findViewById(R.id.loader);
         scrollView=findViewById(R.id.scrollStats);
-
-
-
-
-
-
-
-
         pieChart = (PieChart) findViewById(R.id.piechart);
 
 
-
-        pieChart.addPieSlice(new PieModel("Freetime", 15, Color.parseColor("#FE6DA8")));
-        pieChart.addPieSlice(new PieModel("Sleep", 25, Color.parseColor("#56B7F1")));
-        pieChart.addPieSlice(new PieModel("Work", 35, Color.parseColor("#CDA67F")));
-        pieChart.addPieSlice(new PieModel("Eating", 9, Color.parseColor("#FED70E")));
-
-        pieChart.startAnimation();
         fetchData();
     }
 
@@ -71,16 +60,57 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(String response) {
 
+                        try {
+                            JSONObject jsonObject=new JSONObject(response.toString());
+
+                            tvCases.setText(jsonObject.getString("cases"));
+                            tvRecovered.setText((jsonObject.getString("recovered")));
+                            tvCritical.setText(jsonObject.getString("critical"));
+                            tvActive.setText(jsonObject.getString("active"));
+                            tvTodayCases.setText(jsonObject.getString("todayCases"));
+                            tvTotalDeaths.setText(jsonObject.getString("deaths"));
+                            tvTodayDeaths.setText(jsonObject.getString("todayDeaths"));
+                            tvAffectedCountries.setText(jsonObject.getString("affectedCountries"));
+
+                            pieChart.addPieSlice(new PieModel("Cases",Integer.parseInt(tvCases.getText().toString()),Color.parseColor("#FFA726")));
+                            pieChart.addPieSlice(new PieModel("Recovered",Integer.parseInt(tvRecovered.getText().toString()),Color.parseColor("#66BB6A")));
+                            pieChart.addPieSlice(new PieModel("Deaths",Integer.parseInt(tvTotalDeaths.getText().toString()),Color.parseColor("#EF5350")));
+                            pieChart.addPieSlice(new PieModel("Active",Integer.parseInt(tvActive.getText().toString()),Color.parseColor("#29B6F6")));
+                            pieChart.startAnimation();
+
+                            simpleArcLoader.stop();
+                            simpleArcLoader.setVisibility(View.GONE);
+                            scrollView.setVisibility(View.VISIBLE);
+
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            simpleArcLoader.stop();
+                            simpleArcLoader.setVisibility(View.GONE);
+                            scrollView.setVisibility(View.VISIBLE);
+                        }
+
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
+                simpleArcLoader.stop();
+                simpleArcLoader.setVisibility(View.GONE);
+                scrollView.setVisibility(View.VISIBLE);
                 Toast.makeText(MainActivity.this,error.getMessage(),Toast.LENGTH_SHORT).show();
 
             }
         });
 
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(request);
+
 
     }
 
 }
+
+
+
+
